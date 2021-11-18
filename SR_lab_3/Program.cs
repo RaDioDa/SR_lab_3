babe7ccf4e741fb34e2f4c42c639fd7017feba50
RaDioDa <93476589+RaDioDa@users.noreply.github.com>
2021-11-18 22:18:14 +0300

    Add files via upload

---
+++ SR_lab_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SR_lab_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = 3500;
+            int krozyabra = 65;
+
+            int k = t/(t + (krozyabra*4));
+            Console.WriteLine("K = {0}", k);
+        }
+    }
+}
